8d8e5021dd734520bc9c456e5453a7a4d32fc937
mwongess <amosmwongelah@gmail.com>
2024-01-13 20:05:28 +0300

    format index.js

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 Moyasar.init({
     element: '.mysr-form',
-    // Amount in the smallest currency unit.
-    // For example:
-    // 10 SAR = 10 * 100 Halalas
-    // 10 KWD = 10 * 1000 Fils
-    // 10 JPY = 10 JPY (Japanese Yen does not have fractions)
     amount: 1000,
     currency: 'SAR',
     description: 'Coffee Order #1',
