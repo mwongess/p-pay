2ab59f3b47f562e07e2f0fabf6ceb87c043950c9
mwongess <amosmwongelah@gmail.com>
2024-01-14 21:05:50 +0300

    buy

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ Moyasar.init({
     element: '.mysr-form',
     amount: 1000,
     currency: 'SAR',
-    description: 'Coffee Order #1',
+    description: 'Item to buy',
     publishable_api_key: 'pk_test_AQpxBV31a29qhkhUYFYUFjhwllaDVrxSq5ydVNui',
     callback_url: 'thanks.html',
     methods: ['creditcard']
