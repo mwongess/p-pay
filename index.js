866bc947559513f50e75ffcaa8113968544e5268
mwongess <amosmwongelah@gmail.com>
2024-01-14 21:17:54 +0300

    remove publishable key

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ Moyasar.init({
     amount: 1000,
     currency: 'SAR',
     description: 'Item to buy',
-    publishable_api_key: 'pk_test_AQpxBV31a29qhkhUYFYUFjhwllaDVrxSq5ydVNui',
+    publishable_api_key: '',
     callback_url: 'thanks.html',
     methods: ['creditcard']
   })
